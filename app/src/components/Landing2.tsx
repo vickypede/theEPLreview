d3ec42d1e1b952ced75fc5d89188129b5240f0b3
victor <vonipede@gmail.com>
2025-09-03 19:53:11 -0400

    divider lines redo

--- app/src/components/Landing2.tsx
+++ app/src/components/Landing2.tsx
@@ -111,7 +111,7 @@ export default function Landing2(){
             </div>
             <ul>
               {latestForList.map((a, i) => (
-                <li key={a?.id ?? i} className="px-4 py-4 border-b border-border last:border-0">
+                <li key={a?.id ?? i} className="px-4 py-4 relative after:content-[''] after:absolute after:inset-x-4 after:bottom-0 after:h-px after:bg-border last:after:hidden">
                   {a ? (
                     <Link href={a.url} target="_blank" rel="noopener noreferrer" className="block">
                       <div className="text-xs text-muted-foreground mb-1">{/* timestamp */}
