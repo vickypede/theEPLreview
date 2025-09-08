19e22d0fe4a5ed23783f141d53624a5e927d5264
vickypede <43445443+vickypede@users.noreply.github.com>
2025-09-07 21:40:45 -0400

    feat(ios): integrate FirebaseApp configuration via AppDelegate

---
+++ ios-app/theEPLreview/theEPLreview/AppDelegate.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Firebase
+import FirebaseMessaging
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        UNUserNotificationCenter.current().delegate = self
+        Messaging.messaging().delegate = self
+        application.registerForRemoteNotifications()
+        return true
+    }
+}
--- ios-app/theEPLreview/theEPLreview/theEPLreviewApp.swift
+++ ios-app/theEPLreview/theEPLreview/theEPLreviewApp.swift
@@ -6,27 +6,16 @@
 //
 
 import SwiftUI
-import SwiftData
+import Firebase
 
 @main
 struct theEPLreviewApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+    // Register the AppDelegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
